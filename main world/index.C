c650d4966838cb885d8d3d98f1744c634859dcf1
imangoldfish <imangoldfish@gmail.com>
2025-08-29 16:51:23 +0530

    added more print statements and comments

--- main world/index.C
+++ main world/index.C
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
+int main() {
     // Your code here
+    // dis is a comment
     printf("Hello, world!\n");
+    printf("asdfasdfjk\n");
 }
